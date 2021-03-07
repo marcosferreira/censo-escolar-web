c21d5e67fbe5e263efd3ccb430ad764998be8025
Marcos Ferreira <marcosferreira.developer@gmail.com>
2021-03-07 13:46:09 -0300

    create form input label top animated

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Button, Container, Input, Label } from './styles';
+import logo from '../../assets/logo.png';
 
 export default function SignIn() {
+
+
   return (
     <Container>
-      <h1>Entrar</h1>
+      <form>
+        <img src={logo} alt="logo" />
+        <fieldset>
+          <Input type="text" name="cpf" id="cpf" required />
+          <Label for="cpf">CPF</Label>
+        </fieldset>
+        <fieldset>
+          <Input type="password" name="password" id="password" required  />
+          <Label for="password">SENHA</Label>
+        </fieldset>
+        <Button>Entrar</Button>
+      </form>
     </Container>
   );
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -2,23 +2,92 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  max-width: 900px;
+  min-height: 100vh;
   margin: 0 auto;
+  background-color: #f2f2f2;
+  color: #2957A3;
+
   display: flex;
+  gap: 50px;
   align-items: center;
   justify-content: center;
-  gap: 50px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    background-color: inherit;
+
+    fieldset {
+      width: 100%;
+      background-color: inherit;
+      border: none;
+      position: relative;
+    }
+  }
 `;
 
-export const PanelButton = styled.div`
-  width: 150px;
-  height: 150px;
-  margin-top: 100px;
-  padding: 20px;
-  background-color: #2C5FB0;
-  color: #FFF;
+export const Input = styled.input`
+  width: 300px;
+  margin: 10px 0;
+  padding: 15px;
+  border: 1px solid #CCC;
+  border-radius: 4px;
+  background-color: inherit;
+  font-size: 14px;
 
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  display: block;
+  position: relative;
+
+  &:focus{
+    border: 1px solid #999;
+  }
+
+  &:required:valid {
+    outline: none;
+    background-color: inherit;
+    border: 1px solid #757575;
+  }
+`;
+
+export const Label = styled.label`
+  color: #BBB;
+  font-size: 14px;
+  font-weight: normal;
+  z-index: 1;
+  pointer-events: none;
+
+  position: absolute;
+  left: 16px;
+  top: 25px;
+
+  transition: 0.2s ease all;
+
+  ${Input}:focus ~ & {
+    color: #888;
+  }
+
+  ${Input}:required:valid ~ & {
+    top:4px;
+    font-size:12px;
+    color:#000;
+    background-color: inherit;
+  }
+`;
+
+export const Button = styled.button`
+  margin-top: 10px;
+  padding: 12px 15px;
+  border: none;
+  background-color: #FFF;
+  text-transform: uppercase;
+  box-shadow: 0px 1px 2px #999;
+  transition: 0.4s ease all;
+
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0px 1px 4px #888;
+  }
 `;
